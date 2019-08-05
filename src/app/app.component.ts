d0b48b1aeafec8ac978ae9222c94acdf2984bfd5
sulaeman <sulaeman@rasyid.com>
2019-08-05 21:18:44 +0700

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular6-new';
+  title = 'angular hero';
+  author = 'Sulaeman';
 }
