c2036caea84d6518e90a87fbc86685485f06c4e9
sulaeman <sulaeman@rasyid.com>
2019-08-06 16:55:01 +0700

    ruser

--- src/app/ mock-heroes.ts
+++ src/app/ mock-heroes.ts
@@ -2,7 +2,7 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
+  { id: 12, name: 'Narzzco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
