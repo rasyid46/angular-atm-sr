54578e9b7438449852575e6d1560204b9110350a
sulaeman <sulaeman@rasyid.com>
2019-08-20 10:26:44 +0700

    crud js array

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -5,31 +5,14 @@ import {Employee} from '../model/employee';
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent implements OnInit {
-
-  constructor() {
-
-
-  }
-
-   id= 10;
-  
-
+export class EmployeeComponent {
   private employee: Employee[] = [
     new Employee(1,'Andri','2018-01-02','2000'),
     new Employee(2,'zz','2018-01-02','2000'),
   ];
-
-  ngOnInit() {
-
-  }
-  getEmployee() {
-    return this.employee.slice();
-  }
   
-
   addEmployee(employeeID: number  , employeeName: string, employeeDob: string,employeeSalary : string){
-    let objIndex = this.employee.findIndex((obj => obj.id == employeeID));
+    let objIndex = this.findbyId(employeeID);
     if(objIndex == -1){
       this.employee.push({id : employeeID , name: employeeName , date_born : employeeDob, salary : employeeSalary});
     }else{
@@ -41,13 +24,18 @@ export class EmployeeComponent implements OnInit {
   }
 
   deleteEmpp(id:number){
-    let objIndex = this.employee.findIndex((obj => obj.id == id));
+    let objIndex =this.findbyId(id);
     this.employee.splice(objIndex, 1);
   }
-  
+
   editEmpp(id){
-    let objIndex = this.employee.findIndex((obj => obj.id == id));
+    let objIndex = this.findbyId(id);
     this.employee[objIndex].name = "Laila";
+  }
+
+  findbyId(id){
+    let objIndex = this.employee.findIndex((obj => obj.id == id));
+    return objIndex;
   }
 
 
